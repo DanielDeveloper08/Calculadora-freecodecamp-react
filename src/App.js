a7b2e4216d97342899f2a26a45a2163104a926a6
dalemdev <developerj659@gmail.com>
2023-04-18 20:05:03 -0500

    Calculadora actualizada

--- src/App.js
+++ src/App.js
@@ -1,64 +1,68 @@
-import './App.css';
-import freecodecamplogo from './imagenes/freecodecamp-logo.png';
-import Boton from './componentes/Boton';
-import Pantalla from './componentes/Pantalla';
-import BotonClear from './componentes/BotonClear';
-import { useState } from 'react';
-import { evaluate } from 'mathjs';
+import "./App.css";
+import freecodecamplogo from "./imagenes/freecodecamp-logo.png";
+import Boton from "./componentes/Boton";
+import Pantalla from "./componentes/Pantalla";
+import BotonClear from "./componentes/BotonClear";
+import { useState } from "react";
+import { evaluate } from "mathjs";
 
 function App() {
+  const [input, setInput] = useState("");
 
-  const [input,setInput] = useState('');
-  
-  const agregarInput = val =>{
-    setInput(input + val); 
+  const agregarInput = (valor) => {
+    setInput(input + valor);
   };
 
-  const limpiar = () =>{
-    setInput('');
+  const limpiar = () => {
+    setInput("");
   };
 
-
-  const calcularResultado = () =>{
-    if(input){
+  const calcularResultado = () => {
+    if (input) {
       setInput(evaluate(input));
-    }else{
-      alert('Por favor ingrese valores para realizar los calculos');
+    } else {
+      setInput("Ingrese datos para realizar un calculo");
+      setTimeout(() => {
+        setInput("");
+      }, 1500);
     }
-  };  
-
+  };
   return (
-    <div className='App'>
-      <div className='freecodecamp-logo-contenedor'>
-        <img className='freecodecamp-logo'  src={freecodecamplogo} alt='logo de freecodecamp' />
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          className="freecodecamp-logo"
+          src={freecodecamplogo}
+          alt="logo de freecodecamp"
+        />
       </div>
-      <div className='contenedor-calculadora'>
-        <Pantalla  input={input}/>
-        <div className='fila'>
+      <div className="contenedor-calculadora">
+        <Pantalla input={input} />
+        <div className="fila">
           <Boton manejarClic={agregarInput}>1</Boton>
           <Boton manejarClic={agregarInput}>2</Boton>
           <Boton manejarClic={agregarInput}>3</Boton>
           <Boton manejarClic={agregarInput}>+</Boton>
         </div>
-        <div className='fila'>
+        <div className="fila">
           <Boton manejarClic={agregarInput}>4</Boton>
           <Boton manejarClic={agregarInput}>5</Boton>
           <Boton manejarClic={agregarInput}>6</Boton>
           <Boton manejarClic={agregarInput}>-</Boton>
         </div>
-        <div className='fila'>
+        <div className="fila">
           <Boton manejarClic={agregarInput}>7</Boton>
           <Boton manejarClic={agregarInput}>8</Boton>
           <Boton manejarClic={agregarInput}>9</Boton>
           <Boton manejarClic={agregarInput}>*</Boton>
         </div>
-        <div className='fila'>  
+        <div className="fila">
           <Boton manejarClic={calcularResultado}>=</Boton>
           <Boton manejarClic={agregarInput}>0</Boton>
           <Boton manejarClic={agregarInput}>.</Boton>
           <Boton manejarClic={agregarInput}>/</Boton>
         </div>
-        <div className='fila'>
+        <div className="fila">
           <BotonClear manejarClic={limpiar}>Clear</BotonClear>
         </div>
       </div>
